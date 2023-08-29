f197bc59ea050de38968f6a8bd8a827646e90f78
bwawlstarstop123 <136906034+bwawlstarstop123@users.noreply.github.com>
2023-08-29 20:51:15 +0300

    Add files via upload

---
+++ less9/app_advertisements/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Advertisement
+
+def index(request):
+    ads = Advertisement.objects.all()
+    context = {'advertisements': ads}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def page1(request):
+    return HttpResponse('Успешно! Это обычная страница.')
+
